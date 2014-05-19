4986c16b575c75240db36cb10527af3c2782c751
mobluse <mobluse@users.noreply.github.com>
2014-05-19 13:03:35 +0200

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -2,6 +2,6 @@
 sudo unzip -jo master.zip coderdojo-raspbian-sv-master/boot/config.txt -d /boot
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install links bsdtar
+sudo apt-get install links bsdtar rhino rlwrap
 rm -rf mcpi
 wget -qO- https://s3.amazonaws.com/assets.minecraft.net/pi/minecraft-pi-0.1.1.tar.gz | bsdtar -xvf- -C ~
