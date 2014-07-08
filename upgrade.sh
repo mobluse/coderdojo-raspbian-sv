ce2df7dd189735c250553a193a40b53b26b592cf
mobluse <mobluse@users.noreply.github.com>
2014-07-08 02:11:58 +0200

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo unzip -jo master.zip coderdojo-raspbian-sv-master/boot/config.txt -d /boot
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y upgrade
 sudo apt-get install bsdtar links rhino rlwrap tightvncserver xrdp
 
 # See http://pi.minecraft.net/ & http://www.minecraftforum.net/forum/216-minecraft-pi-edition/
@@ -11,3 +11,13 @@ wget -qO- https://s3.amazonaws.com/assets.minecraft.net/pi/minecraft-pi-0.1.1.ta
 # See http://www.raspberrypi.org/forums/viewtopic.php?p=503683#p503683
 wget https://dl.dropbox.com/s/zgkq2jw9o9wbava/installBYOB.sh -O installBYOB.sh
 sudo bash installBYOB.sh
+
+# See http://www.raspberrypi.org/forums/viewtopic.php?f=78&t=69420&p=551155
+wget -O c64.zip 'https://docs.google.com/uc?authuser=0&id=0B51Q7dpulGC8dEVtTk5ReFBLX0U&export=download'
+unzip c64.zip
+cd ~/c64
+sudo mv vice /usr/local/lib/
+chmod +x x64
+sudo mv x64 /usr/local/bin/
+# x64 -sdlbitdepth 8
+# Save settings within VICE, then start with x64
