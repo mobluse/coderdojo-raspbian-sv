77d9462ef222fb95c9643e6bcbfebee1e2c6dced
mobluse <mobluse@users.noreply.github.com>
2014-07-15 21:14:24 +0200

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -2,7 +2,7 @@
 sudo unzip -jo master.zip coderdojo-raspbian-sv-master/boot/config.txt -d /boot
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get install bsdtar links rhino rlwrap tightvncserver xrdp
+sudo apt-get install bsdtar links rhino rlwrap tightvncserver xrdp avahi-daemon avahi-autoipd freepats
 
 # See http://pi.minecraft.net/ & http://www.minecraftforum.net/forum/216-minecraft-pi-edition/
 rm -rf mcpi
@@ -23,3 +23,12 @@ sudo mv x64 /usr/local/bin/
 # Save settings within VICE (F12/Settings management/Save current settings), then start with 
 # x64
 wget 'http://c64.orbin.se/Comal 80 (1985)(Commodore)[a].crt'
+cd
+
+# See http://www.raspberrypi.org/forums/viewtopic.php?p=534518#p534518
+wget http://df.lth.se.orbin.se/~mikaelb/abc/80/abc80sim-2.1-raspi.tar.gz
+tar xvf abc80sim-2.1-raspi.tar.gz
+mv abc80sim-2.1-raspi abc80sim-2.1
+cd ~/abc80sim-2.1/abcdir/
+wget http://df.lth.se.orbin.se/~mikaelb/abc/80/masken.bas
+cd
